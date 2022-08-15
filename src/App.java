2eb9d30f44c52f1ffa1a3d0a4863ae4ab9540da0
Jimmy Valencia Urbano <jvalu90@gmail.com>
2022-08-15 14:56:10 -0500

    Creation of functions

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+      
+    }
+
+    public static int[][] readMatrix(int m, int n) {
+      int[][] matrix = new int[m][n]; 
+
+      return matrix;
+    }
+
+    public static int[] readArray(int n) {
+      // Scanner
+      int[] array = new int[n];
+
+      for (int i = 0; i < n; i++) {
+        System.out.print("Please type the code of the product " + (i + 1) + ": ");
+      }
+
+      return array; 
     }
 }
