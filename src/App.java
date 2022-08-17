bdef439bafa9573f79b16df918af5d7a3cd95335
Jimmy Valencia Urbano <jvalu90@gmail.com>
2022-08-16 20:29:36 -0500

    read Array function updated

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+
+import javax.naming.event.NamingExceptionEvent;
+
 import java.util.Locale;
 
 public class App {
@@ -13,13 +16,19 @@ public class App {
     }
 
     public static int[] readArray(int n) {
-      // Scanner
-      int[] array = new int[n];
+      
+      Scanner inputs = new Scanner(System.in).useLocale(Locale.US);    
 
+      
+      int[] array = new int[n];
+        
       for (int i = 0; i < n; i++) {
-        System.out.print("Please type the code of the product " + (i + 1) + ": ");
+          System.out.print("Please type the code of the product " + (i + 1) + ": ");
+          array[i] = inputs.nextInt();
       }
 
-      return array; 
+      return array;
+
     }
+
 }
